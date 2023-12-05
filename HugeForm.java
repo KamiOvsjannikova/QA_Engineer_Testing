7a07e37215e22fb225fc589b43f94294861c0f3d
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-12-05 21:39:44 +0200

    Update HugeForm.java

--- HugeForm.java
+++ HugeForm.java
@@ -23,20 +23,19 @@ public class HugeForm {
     @BeforeEach
     public void setup() {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\kamil\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        //ChromeDriver driver = new ChromeDriver();//// importirovali chrome driver cherez alt+enter
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // kazdie 5 sek, dlja vsex elementov
+        //ChromeDriver driver = new ChromeDriver();
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); 
         driver = new ChromeDriver();
         driver.get("http://suninjuly.github.io/huge_form.html");
     }
 
-    @AfterEach//metod ,kotorij pozvoljaet zakrivatj brauzer posle kazhdogo testa, dazhe ,esli test padaet
+    @AfterEach
     public void tearDown() {
         driver.quit();
     }
 
     @Test
     public void successfulRegistration(){
-        //vse polja dlja vvoda najdem //input//kolekcija web elemenot
         List<WebElement> inputFields =
                 driver.findElements(By.tagName("input"));
         //enter data to all input fields
@@ -49,12 +48,11 @@ public class HugeForm {
 
         //Congrats, you've passed the task!
         //zdatj vremja
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20)); // novoe ozidanija v tech 20 sek maximal kol-vo vremeni
-        Alert alert = wait.until(ExpectedConditions.alertIsPresent()) ;// zdatj do tex por pokaAlert ne stanet videlennim
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
         System.out.println(alert.getText());
 
         //check that alert has text "Congrats, you've passed the task!"
         assertTrue(alert.getText().contains("Congrats, you've passed the task!"));
     }
-
 }
